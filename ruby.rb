761615bd66641b7342113df594fb2cccceafff22
miquelpuigmena <miquelpuigmena@gmail.com>
2018-04-24 14:10:33 +0200

    Update ruby.rb

--- ruby.rb
+++ ruby.rb
@@ -45,30 +45,30 @@ class Calculator
         operator_stack=[]
         @expression.each{|cell|
             case cell
-            when Is_numeric
-        puts "num"
-                output_queue.push(cell)
-            when Function_calculator, "("
-        puts "funct or ("
-                operator_stack.push(cell)
-            when Operator_calculator
-        puts "Operator"
-                operator_stack.each_with_index { |operator, index|
-            puts "in each operate stack"
-            puts operator, index
-                    operator_stack[index] = ""
-                    break if ((accepted_functions.include? operator or getInfoFromHash(operator, "precedence")>getInfoFromHash(cell, "precedence") or (getInfoFromHash(operator, "precedence")==getInfoFromHash(cell, "precedence") and getInfoFromHash(operator, "associativity")>getInfoFromHash(cell, "associativity"))) and operator != "(")
-                    output_queue.push(operator)
-                }
-                operator_stack.push(cell)
-            when ")"
-        puts ")"
-                operator_stack.each { |operator, index|
-                    operator_stack[index] = ""
-                    break if operator == "("
-                    output_queue.push(operator)
-                }
-                operator_stack.reject(&:empty?)
+                when Is_numeric
+                    puts "num"
+                    output_queue.push(cell)
+                when Function_calculator, "("
+                    puts "funct or ("
+                    operator_stack.push(cell)
+                when Operator_calculator
+                    puts "Operator"
+                    operator_stack.each_with_index { |operator, index|
+                        puts "in each operate stack"
+                        puts operator, index
+                        operator_stack[index] = ""
+                        break if ((accepted_functions.include? operator or getInfoFromHash(operator, "precedence")>getInfoFromHash(cell, "precedence") or (getInfoFromHash(operator, "precedence")==getInfoFromHash(cell, "precedence") and getInfoFromHash(operator, "associativity")>getInfoFromHash(cell, "associativity"))) and operator != "(")
+                        output_queue.push(operator)
+                    }
+                    operator_stack.push(cell)
+                when ")"
+                    puts ")"
+                    operator_stack.each { |operator, index|
+                        operator_stack[index] = ""
+                        break if operator == "("
+                        output_queue.push(operator)
+                    }
+                    operator_stack.reject(&:empty?)
             end
         }
         operator_stack.each { |operator| output_queue.push(operator) }
